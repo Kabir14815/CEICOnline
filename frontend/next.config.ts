2c646f70539330b1a59f5c86e4d17e33102838e4
Kabir Lal <kabirbhalla3.90@gmail.com>
2026-01-11 17:23:41 +0530

    Refactor frontend to use BACKEND_URL env var

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
+        destination: process.env.BACKEND_URL ? `${process.env.BACKEND_URL}/:path*` : 'http://127.0.0.1:8000/:path*',
       },
     ];
   },
